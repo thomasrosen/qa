261dd2fbc1dcc97a433f89abfa310e8951778f2c
thomasrosen <thomas.rosen@me.com>
2024-03-10 20:20:36 +0100

    InputForm can't be exported here

--- src/app/suggest/page.tsx
+++ src/app/suggest/page.tsx
@@ -20,7 +20,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 
-export function InputForm() {
+function InputForm() {
   const form = useForm<QuestionSchemaType>({
     resolver: zodResolver(QuestionSchema),
     defaultValues: {
