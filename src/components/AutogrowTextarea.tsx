892daab753f9cb5315369179e7b8b60f4ee6d283
thomasrosen <thomas.rosen@me.com>
2024-03-31 18:00:58 +0200

    AutoGrowTextarea

---
+++ src/components/AutogrowTextarea.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useRef } from 'react'
+import { resize } from 'react-expanding-textarea'
+import { Textarea } from './ui/textarea'
+
+import * as React from 'react'
+
+export interface AutoGrowTextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const AutoGrowTextarea = React.forwardRef<HTMLTextAreaElement, AutoGrowTextareaProps>(
+  ({ onChange, ...props }, ref) => {
+    const refTextarea = useRef<HTMLTextAreaElement>(null)
+
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+      resize(0, refTextarea.current)
+      onChange?.(event)
+    }
+
+    return (
+      <Textarea
+        {...props}
+        className="min-h-[0px] resize-none"
+        rows={1}
+        ref={refTextarea}
+        onChange={handleChange}
+      />
+    )
+  }
+)
+AutoGrowTextarea.displayName = 'AutoGrowTextarea'
+
+export { AutoGrowTextarea }
