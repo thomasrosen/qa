8bc680efae4aec872758d50e6e4eba732bbf14b6
thomasrosen <thomas.rosen@me.com>
2024-04-12 18:20:20 +0200

    no shadow for the cards

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -10,10 +10,7 @@ const Card = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn(
-      'rounded-lg bg-card text-card-foreground shadow-sm',
-      className
-    )}
+    className={cn('rounded-lg bg-card text-card-foreground', className)}
     {...props}
   />
 ))
