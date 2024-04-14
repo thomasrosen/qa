67dad8778599db167d7a968ee3eef1de6cb069e9
thomasrosen <thomas.rosen@me.com>
2024-04-14 21:46:01 +0200

    fix classname in AutoGrowTextarea

--- src/components/client/AutogrowTextarea.tsx
+++ src/components/client/AutogrowTextarea.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import { Textarea } from '@/components/ui/textarea'
-import { useRef } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 import { resize } from 'react-expanding-textarea'
 
+import { cn } from '@/lib/utils'
 import * as React from 'react'
 
 export interface AutoGrowTextareaProps
@@ -12,18 +13,27 @@ export interface AutoGrowTextareaProps
 const AutoGrowTextarea = React.forwardRef<
   HTMLTextAreaElement,
   AutoGrowTextareaProps
->(({ onChange, ...props }, ref) => {
+>(({ onChange, className, ...props }, ref) => {
   const refTextarea = useRef<HTMLTextAreaElement>(null)
 
-  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    resize(0, refTextarea.current)
-    onChange?.(event)
-  }
+  const handleChange = useCallback(
+    (event?: React.ChangeEvent<HTMLTextAreaElement>) => {
+      resize(0, refTextarea.current)
+      if (event) {
+        onChange?.(event)
+      }
+    },
+    [onChange]
+  )
+
+  useEffect(() => {
+    handleChange()
+  }, [handleChange])
 
   return (
     <Textarea
       {...props}
-      className="min-h-[0px] resize-none"
+      className={cn('min-h-[0px] resize-none', className)}
       rows={1}
       ref={refTextarea}
       onChange={handleChange}
