04645fa0623f70931a28e1aa7d820bfd9f3178a6
thomasrosen <thomas.rosen@me.com>
2024-04-14 07:54:29 +0200

    wiggle animation (if ever needed)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,10 +74,24 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        wiggle: {
+          '0%, 100%': {
+            transform: 'rotate(-1deg)',
+          },
+          '50%': { transform: 'rotate(1.5deg)' },
+        },
+        wiggle2: {
+          '0%, 100%': {
+            transform: 'rotate(-1.5deg)',
+          },
+          '50%': { transform: 'rotate(2deg)' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        wiggle: 'wiggle 0.25s ease-in-out infinite', // source: https://www.kirupa.com/html5/creating_the_ios_icon_jiggle_wobble_effect_in_css.htm
+        wiggle2: 'wiggle2 0.25s ease-in-out infinite',
       },
     },
   },
