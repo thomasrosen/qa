69e821e8cad5a5e20b143970cb80ab9ea8754ced
thomasrosen <thomas.rosen@me.com>
2024-04-17 00:20:34 +0200

    allow any Paragraph properties

--- src/components/P.tsx
+++ src/components/P.tsx
@@ -1,49 +1,46 @@
 import { cn } from '@/lib/utils'
 
 export function P({
-  children,
   type,
-  className,
+  className = '',
   ref,
+  ...props
 }: {
-  children?: React.ReactNode
   type?: 'lead' | 'large' | 'small' | 'muted' | 'ghost' | undefined
-  className?: string
   ref?: React.Ref<HTMLParagraphElement>
-}) {
+} & React.HTMLAttributes<HTMLParagraphElement>) {
   switch (type) {
     case 'lead':
       return (
         <p
           ref={ref}
           className={cn('text-xl text-muted-foreground mb-4', className)}
-        >
-          {children}
-        </p>
+          {...props}
+        />
       )
     case 'large':
       return (
-        <p ref={ref} className={cn('text-lg font-semibold mb-4', className)}>
-          {children}
-        </p>
+        <p
+          ref={ref}
+          className={cn('text-lg font-semibold mb-4', className)}
+          {...props}
+        />
       )
     case 'small':
       return (
         <p
           ref={ref}
           className={cn('text-sm font-medium leading-none mb-4', className)}
-        >
-          {children}
-        </p>
+          {...props}
+        />
       )
     case 'muted':
       return (
         <p
           ref={ref}
           className={cn('text-sm text-muted-foreground mb-4', className)}
-        >
-          {children}
-        </p>
+          {...props}
+        />
       )
     case 'ghost':
       return (
@@ -53,15 +50,10 @@ export function P({
             'text-sm text-muted-foreground mb-4 opacity-60',
             className
           )}
-        >
-          {children}
-        </p>
+          {...props}
+        />
       )
   }
 
-  return (
-    <p ref={ref} className={cn('leading-7 mb-4', className)}>
-      {children}
-    </p>
-  )
+  return <p ref={ref} className={cn('leading-7 mb-4', className)} {...props} />
 }
