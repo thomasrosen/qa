c4880adab8d0c679cd84e67eaf7cedd96671bc09
thomasrosen <thomas.rosen@me.com>
2024-05-04 17:13:58 +0200

    fixed loading the next question

--- src/components/QuestionPageContent.tsx
+++ src/components/QuestionPageContent.tsx
@@ -4,7 +4,7 @@ import { preloadAnswersForQuestion } from '@/actions/preloadAnswersForQuestion'
 import { preloadQuestion } from '@/actions/preloadQuestion'
 import { Headline } from '@/components/Headline'
 import { QuestionCard } from '@/components/client/QuestionCard'
-import { PreloadedAnswer } from '@/lib/types'
+import { PreloadedAnswer, QuestionSchemaType } from '@/lib/types'
 import { cn } from '@/lib/utils'
 import { TC } from '@/translate/components/client/TClient'
 import Link from 'next/link'
@@ -29,18 +29,26 @@ export function QuestionPageContent({
   preloadedAnswers?: PreloadedAnswer[]
   noQuestionsFallback?: React.ReactNode
 }) {
-  const preloadedQuestionCache = useRef(preloadedQuestion)
-  const preloadedAnswersCache = useRef(preloadedAnswers)
+  const nextQuestionCache = useRef<QuestionSchemaType | null>(null)
+  const nextAnswersCache = useRef<PreloadedAnswer[]>([])
+
+  const currentQuestionCache = useRef<QuestionSchemaType | null>(
+    preloadedQuestion
+  )
+  const currentAnswersCache = useRef<PreloadedAnswer[]>(preloadedAnswers)
+
+  const prevQuestionCache = useRef<QuestionSchemaType | null>(null)
+  const prevAnswersCache = useRef<PreloadedAnswer[]>([])
 
   const loadingNextRef = useRef(false)
   const showNextFunctionCache = useRef<(() => void) | undefined>(undefined)
   const [isLoadingNextQuestion, setIsLoadingNextQuestion] = useState(false)
 
   const [question, setQuestion] = useState(preloadedQuestion)
-  const [answers, setAnswers] = useState(preloadedAnswers)
-  const [hasPreloadedQuestion, setHasPreloadedQuestion] = useState(
-    !!preloadedQuestion
-  )
+  const [answers, setAnswers] = useState<PreloadedAnswer[]>([])
+  // const [hasPreloadedQuestion, setHasPreloadedQuestion] = useState(
+  //   !!preloadedQuestion
+  // )
 
   const aboutThing = preloadedAboutThing
 
@@ -48,30 +56,29 @@ export function QuestionPageContent({
     if (loadingNextRef.current === false) {
       loadingNextRef.current = true
 
-      preloadAnswersForQuestion(
-        preloadedQuestionCache.current?.question_id || undefined
-      )
-        .then((newAnswers) => {
-          preloadedAnswersCache.current = newAnswers
-        })
-        .catch((error) => {
-          console.error('preloadAnswersForQuestion error', error)
-        })
+      // load question
+      const nextQuestion = await preloadQuestion()
+      nextQuestionCache.current = nextQuestion
+      // setHasPreloadedQuestion(!!nextQuestionCache.current)
 
-      preloadQuestion()
-        .then((newQuestion) => {
-          preloadedQuestionCache.current = newQuestion
-          setHasPreloadedQuestion(!!preloadedQuestionCache.current)
+      // load answers
+      if (nextQuestion && nextQuestion?.question_id) {
+        const nextAnswers = await preloadAnswersForQuestion(
+          nextQuestion.question_id
+        )
+        nextAnswersCache.current = nextAnswers
+      } else {
+        nextAnswersCache.current = []
+      }
 
-          loadingNextRef.current = false
-          if (typeof showNextFunctionCache.current === 'function') {
-            showNextFunctionCache.current()
-            showNextFunctionCache.current = undefined
-          }
-        })
-        .catch((error) => {
-          console.error('preloadQuestion error', error)
-        })
+      // loading finished
+      loadingNextRef.current = false
+
+      // set question if caught while loading
+      if (typeof showNextFunctionCache.current === 'function') {
+        showNextFunctionCache.current()
+        showNextFunctionCache.current = undefined
+      }
     }
   }, [])
 
@@ -82,39 +89,47 @@ export function QuestionPageContent({
   }, [preloadNext])
 
   const showNext = useCallback(async () => {
-    function showNextQuestion() {
-      if (preloadedQuestionCache.current) {
-        setQuestion(preloadedQuestionCache.current)
-        preloadNext()
-
-        const nextQuestionId = preloadedQuestionCache.current?.question_id || ''
-
-        if (window) {
-          if (nextQuestionId) {
-            window.history.pushState(null, '', `/q/${nextQuestionId}`)
-          } else {
-            window.history.pushState(null, '', '/q')
-          }
-        }
-      } else {
-        setQuestion(undefined)
-      }
-
-      if (preloadedAnswersCache.current) {
-        setAnswers(preloadedAnswersCache.current)
-      } else {
-        setAnswers(preloadedAnswersCache.current || [])
-      }
+    async function showNextQuestion() {
       setIsLoadingNextQuestion(false)
+
+      // shift all caches
+      prevQuestionCache.current = currentQuestionCache.current || null
+      prevAnswersCache.current = currentAnswersCache.current || []
+
+      currentQuestionCache.current = nextQuestionCache.current || null
+      currentAnswersCache.current = nextAnswersCache.current || []
+
+      nextQuestionCache.current = null // new values will be set in preloadNext
+      nextAnswersCache.current = [] // new values will be set in preloadNext
+
+      // set values that will be displayed
+      setQuestion(currentQuestionCache.current)
+      setAnswers(prevAnswersCache.current)
+
+      if (
+        currentQuestionCache.current &&
+        currentQuestionCache.current?.question_id &&
+        window
+      ) {
+        const currentQuestionId = currentQuestionCache.current?.question_id
+        if (embedded === true) {
+          window.history.pushState(null, '', `/embed/q/${currentQuestionId}`)
+        } else {
+          window.history.pushState(null, '', `/q/${currentQuestionId}`)
+        }
+      }
+
+      // get real values for nextQuestionCache and nextAnswersCache
+      await preloadNext()
     }
 
-    if (loadingNextRef.current) {
+    if (loadingNextRef.current === true) {
       showNextFunctionCache.current = showNextQuestion
       setIsLoadingNextQuestion(true)
     } else {
       showNextQuestion()
     }
-  }, [preloadNext])
+  }, [embedded, preloadNext])
 
   const questionNeedsAboutThing =
     question && question.aboutThingTypes && question.aboutThingTypes.length > 0
@@ -143,6 +158,7 @@ export function QuestionPageContent({
       </section>
     )
   }
+
   return (
     <>
       {isLoadingNextQuestion && (
@@ -181,49 +197,50 @@ export function QuestionPageContent({
       {!displayQuestion && !answerIsForPreloadedQuestion && noQuestionsFallback}
 
       {hasAnswers && (
-        <section className="flex flex-col gap-4 mb-4 place-content-center">
-          <div className="mb-2 mt-8 flex justify-between items-center gap-4">
-            <Headline type="h2" className="border-0 p-0 m-0">
-              <TC
-                keys="answerChartWrapper"
-                key={String(answerIsForPreloadedQuestion)}
-              >
-                {answerIsForPreloadedQuestion
-                  ? 'Ergebnisse für diese Frage'
-                  : 'Ergebnisse zu deiner letzte Antwort'}
-              </TC>
-            </Headline>
-            <Link href="/answers">
-              <Button variant="outline">
-                <TC keys="answerChartWrapper">Alle Antworten</TC>
-              </Button>
-            </Link>
-          </div>
-          {answerIsForPreloadedQuestion && displayQuestion ? (
-            <P>Die Antwort siehst du nachdem beantworten.</P>
-          ) : (
-            answers
-              .filter(Boolean)
-              .map((answerData) => (
-                <AnswerChart
-                  key={answerData.answer.answer_id}
-                  answer={answerData.answer}
-                  amountOfAnswers={answerData.amountOfAnswers}
-                  newestValueDate={answerData.newestValueDate}
-                  values={answerData.values}
-                />
-              ))
+        <section
+          className={cn(
+            'flex flex-col gap-4 mb-4 place-content-center',
+            !(answerIsForPreloadedQuestion && displayQuestion) && 'items-center'
           )}
+        >
+          <Headline type="h2" className="border-0 mt-8 p-0 mb-0">
+            <TC
+              keys="answerChartWrapper"
+              key={String(answerIsForPreloadedQuestion)}
+            >
+              {answerIsForPreloadedQuestion
+                ? 'Ergebnisse für diese Frage'
+                : 'Ergebnisse zu deiner letzte Antwort'}
+            </TC>
+          </Headline>
+          {/* {answerIsForPreloadedQuestion && displayQuestion ? (
+            <P className="m-0">Die Antwort siehst du nach dem Beantworten.</P>
+          ) : ( */}
+          {answers.filter(Boolean).map((answerData) => (
+            <AnswerChart
+              key={answerData.answer.answer_id}
+              answer={answerData.answer}
+              amountOfAnswers={answerData.amountOfAnswers}
+              newestValueDate={answerData.newestValueDate}
+              values={answerData.values}
+            />
+          ))}
+          {/* )} */}
+          <Link href="/answers">
+            <Button variant="outline">
+              <TC keys="answerChartWrapper">Alle Antworten</TC>
+            </Button>
+          </Link>
         </section>
       )}
 
-      {!displayQuestion &&
-        answerIsForPreloadedQuestion &&
-        hasPreloadedQuestion && (
-          <div className="flex flex-row justify-center mt-8">
-            <Button onClick={showNext}>Nächste Frage beantworten</Button>
-          </div>
-        )}
+      {!displayQuestion && answerIsForPreloadedQuestion && (
+        <div className="flex flex-row justify-center mt-8">
+          <Link href="/q">
+            <Button>Nächste Frage beantworten</Button>
+          </Link>
+        </div>
+      )}
     </>
   )
 }
