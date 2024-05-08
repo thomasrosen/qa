2d9a668563c552caba58df0c5bbd50ce25df8642
thomasrosen <thomas.rosen@me.com>
2024-05-09 01:21:08 +0200

    fixed getSimilarQuestions

--- src/lib/getSimilarQuestions.ts
+++ src/lib/getSimilarQuestions.ts
@@ -1,4 +1,4 @@
-import { QuestionSchemaType, prisma } from '@/lib/prisma'
+import { QuestionSchemaType, prisma, type Prisma } from '@/lib/prisma'
 import { addContent, createVectorStore, similarTexts } from '@/lib/vectorStore'
 
 const vectorStore = createVectorStore()
@@ -25,18 +25,40 @@ async function addAllQuestionsToVectorStore() {
 export async function getSimilarQuestions({
   q,
   amount,
+  threshold = 0.2,
+  where = {},
+  select = undefined,
+  include = undefined,
 }: {
   q: string
   amount: number
+  threshold?: number
+  where?: Prisma.QuestionWhereInput
+  select?: Prisma.QuestionSelect
+  include?: Prisma.QuestionInclude
 }): Promise<QuestionSchemaType[]> {
+  const amountTimesTwo = amount * 2 // amount * 2 to get more than we need
+
   const result =
-    (await similarTexts({ vectorStore, q, amount: amount * 2 })) || [] // amount * 2 to get more than we need
+    (await similarTexts({ vectorStore, q, amount: amountTimesTwo })) || []
 
   const foundQuestions = result
-    .filter((r) => (r.metadata._distance || 1) <= 0.2)
+    .filter((r) => (r.metadata._distance || 0) <= threshold)
     .map((r) => r.pageContent)
 
-  const questions =
+  if (!select && !include) {
+    include = {
+      Answer_isAnswering: {
+        include: {
+          values: true,
+          context: true,
+        },
+      },
+      // context: true,
+    }
+  }
+
+  let questions: QuestionSchemaType[] =
     (await prisma.question.findMany({
       relationLoadStrategy: 'join', // or 'query'
       where: {
@@ -44,23 +66,27 @@ export async function getSimilarQuestions({
         question: {
           in: foundQuestions,
         },
+        ...where,
       },
-      take: amount,
-      // select: {
-      //   question_id: true,
-      //   question: true,
-      //   description: true,
-      // },
-      include: {
-        Answer_isAnswering: {
-          include: {
-            values: true,
-            context: true,
-          },
-        },
-        // context: true,
-      },
+      take: amountTimesTwo,
+      select,
+      include,
     })) || []
+
+  questions = questions
+    .filter(Boolean)
+    .map((q) => {
+      const found = result.find((r) => r.pageContent === q.question)
+      if (found) {
+        q._distance = found.metadata._distance
+      } else {
+        q._distance = 1
+      }
+
+      return q
+    })
+    .sort((a, b) => (a._distance || 1) - (b._distance || 1))
+    .slice(0, amount)
 
   // const preloadedAnswer = await preloadAnswersForQuestion(question_id)
 
